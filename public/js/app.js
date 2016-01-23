326710a0c87c30e547cb051a55d059f693be587b
Zachary Smith <billyzacsmith@gmail.com>
2016-01-23 15:35:55 -0700

    GET the pxon data from my server, and put some pixels on the DOM. Right now, they just come out as a single row, but at least the pieces are connected.

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,24 @@ var app = angular.module('app', [])
 // var pxonServerUrl = 'http://localhost:3030/'
 var pxonServerUrl = 'https://pxon.herokuapp.com/'
 
-
-app.controller('MainController', function($scope) {
-  $scope.row = []
-  for (var i = 0; i < 10; i++) {
-    $scope.row.push({
-      id: 'pixel-' + i,
-      color: 'tomato'
+app.controller('MainController', function($scope, $http) {
+  $http({
+    method: 'GET',
+    url: pxonServerUrl
+  })
+  .then(function(pxonData) {
+    $scope.row = []
+    for (var i = 0; i < 150; i++) {
+      $scope.row.push({
+        id: i,
+        color: 'rgba(200, 0, 10, 0.1)'
+      })
+    }
+    console.log(pxonData.data.pxif.pixels)
+    // List the pixels:
+    pxonData.data.pxif.pixels.forEach(function(pixel) {
+      console.log('x:' + pixel.x + '  y:' + pixel.y)
+      $scope.row[pixel.x].color = pixel.color
     })
-  }
+  })
 })
