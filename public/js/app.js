d484fbcad227ace6f6924c2abcf5ae06f214524e
Zachary Smith <billyzacsmith@gmail.com>
2016-01-23 10:08:32 -0700

    Wire up basic angular app.

--- public/js/app.js
+++ public/js/app.js
@@ -1,35 +1,8 @@
-var canvas = document.getElementById('canvas')
-var ctx = canvas.getContext('2d')
+var app = angular.module('app', [])
+
 // var pxonServerUrl = 'http://localhost:3030/'
 var pxonServerUrl = 'https://pxon.herokuapp.com/'
 
-$.get(pxonServerUrl, function (pxonData) {
-  console.log('got the pxonz: ' + pxonData)
-  pxonData.pxif.pixels.forEach(function(e, i, a){
-    drawPixel(e.x, e.y, e.color, e.size );
-  })
+app.controller('MainController', function($scope) {
+  $scope.something = 'Foo'
 })
-
-// drawPixel(50, 50, 'rgba(100, 20, 200, 0.5)', 20)
-
-function drawPixel (x, y, color, size) {
-    console.log('drawing it.')
-    ctx.beginPath();
-    x = ( Math.ceil(x/size) * size ) - size;
-    y = ( Math.ceil(y/size) * size ) - size;
-    ctx.moveTo (x, y);
-    ctx.fillStyle = color;
-    ctx.lineHeight = 0;
-
-    if ( color === 'rgba(0, 0, 0, 0)' ) {
-      ctx.clearRect(x,y,size,size);
-    }
-    else {
-      ctx.fillRect(x,y,size,size);
-    }
-
-    return {
-      x: x,
-      y: y
-    };
-  };
