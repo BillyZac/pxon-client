2fc2f64da485ef981f70cdd1f0a00364a171cc40
Zachary Smith <billyzacsmith@gmail.com>
2016-01-23 16:34:54 -0700

    Put the pxon onto the DOM!!!

--- public/js/app.js
+++ public/js/app.js
@@ -9,35 +9,35 @@ app.controller('MainController', function($scope, $http) {
     url: pxonServerUrl
   })
   .then(function(pxonData) {
-    var matrix = []
 
+    // Initialize the matrix with blank pixels
+    var matrix = []
     for (var y=0; y<130; y++) {
       var row = []
-      for (var x=0; x<130; x++) {
+      for (var x=0; x<150; x++) {
         var blankPixel = {
           x: x,
           y: y,
-          color: "rgba(0, 80, 30, 0.1)",
+          color: "rgba(0, 0, 0, 0)",
           size: 15}
         row.push(blankPixel)
       }
       matrix.push(row)
     }
 
-    // var i = 0
-    // pxonData.data.pxif.pixels.forEach(function(pixel) {
-    //   i++
-    //   matrix[pixel.y][pixel.y] = pixel
-    // })
+    // Put the pixels in the pxon in the matrix
+    pxonData.data.pxif.pixels.forEach(function(pixel) {
+      matrix[pixel.y][pixel.x] = pixel
+    })
 
     // Testing:
-    // for (var y=0; y<130; y++) {
-    //   for (var x=0; x<130; x++) {
-    //     if (matrix[x][y].color != "rgba(0, 0, 0, 0)") {
-    //       console.log(matrix[x][y])
-    //     }
-    //   }
-    // }
+    for (var y=0; y<130; y++) {
+      for (var x=0; x<130; x++) {
+        if (matrix[x][y].color != "rgba(0, 0, 0, 0)") {
+          console.log(matrix[x][y])
+        }
+      }
+    }
 
     $scope.matrix = matrix
   })
