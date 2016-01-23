7821a5bc899993fd8ec9f7c303ec5399a6253b9f
Zachary Smith <billyzacsmith@gmail.com>
2016-01-23 10:38:18 -0700

    Display a row of 'pixels' using Angular.

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,13 @@ var app = angular.module('app', [])
 // var pxonServerUrl = 'http://localhost:3030/'
 var pxonServerUrl = 'https://pxon.herokuapp.com/'
 
+
 app.controller('MainController', function($scope) {
-  $scope.something = 'Foo'
+  $scope.row = []
+  for (var i = 0; i < 10; i++) {
+    $scope.row.push({
+      id: 'pixel-' + i,
+      color: 'tomato'
+    })
+  }
 })
