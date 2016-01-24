f6fb994e94d6f374db948145b32c97d96c7adaeb
Zachary Smith <billyzacsmith@gmail.com>
2016-01-23 17:04:16 -0700

    Make a chunkier version of the image. Add some parameterization, to make tweaking easier.

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ var app = angular.module('app', [])
 
 // var pxonServerUrl = 'http://localhost:3030/'
 var pxonServerUrl = 'https://pxon.herokuapp.com/'
+var scalingFactor = 0.25 // Ratio between original pixel dimensions and the "pixelated" image we're about to show in the DOM
+var pixelWidth = 50 // Using this for width and height of the resulting image
 
 app.controller('MainController', function($scope, $http) {
   $http({
@@ -12,9 +14,9 @@ app.controller('MainController', function($scope, $http) {
 
     // Initialize the matrix with blank pixels
     var matrix = []
-    for (var y=0; y<130; y++) {
+    for (var y=0; y<pixelWidth; y++) {
       var row = []
-      for (var x=0; x<150; x++) {
+      for (var x=0; x<pixelWidth; x++) {
         var blankPixel = {
           x: x,
           y: y,
@@ -27,12 +29,14 @@ app.controller('MainController', function($scope, $http) {
 
     // Put the pixels in the pxon in the matrix
     pxonData.data.pxif.pixels.forEach(function(pixel) {
-      matrix[pixel.y][pixel.x] = pixel
+      var x = Math.ceil(pixel.x * scalingFactor)
+      var y = Math.ceil(pixel.y * scalingFactor)
+      matrix[y][x] = pixel
     })
 
     // Testing:
-    for (var y=0; y<130; y++) {
-      for (var x=0; x<130; x++) {
+    for (var y=0; y<pixelWidth; y++) {
+      for (var x=0; x<pixelWidth; x++) {
         if (matrix[x][y].color != "rgba(0, 0, 0, 0)") {
           console.log(matrix[x][y])
         }
